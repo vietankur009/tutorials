d2a8085d679f7a128e85b7f5d9576b725fefa967
Ankit Chauhan <advik.ankitchauhan85@gmail.com>
2019-02-18 20:13:55 +0530

    commit after changes

--- java-tutorial/src/main/java/com/lt/core/questions/TemperatureConverter.java
+++ java-tutorial/src/main/java/com/lt/core/questions/TemperatureConverter.java
@@ -3,13 +3,12 @@ package main.java.com.lt.core.questions;
 public class TemperatureConverter {
 
     public static void main(String... arg) {
-        celsiusToFahrenheit();
+        celsiusToFahrenheit(10.0);
     }
 
-    public static void celsiusToFahrenheit() {
+    public static void celsiusToFahrenheit(double fahrenheit) {
 
-        double fahrenheit, celsius;
-        fahrenheit = 10;
+        double celsius;
         celsius = fahrenheit * 32;
         System.out.println(fahrenheit + " Degree Fahrenheit =   " + celsius + " Degree Celsius");
     }
