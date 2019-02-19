14e5b52d6b8bfcfbde46f08b3c935fd78972fd70
Ankit Chauhan <advik.ankitchauhan85@gmail.com>
2019-02-19 09:52:02 +0530

    commit after change

---
+++ java-tutorial/src/main/java/com/lt/core/questions/Temperature.java
@@ -0,0 +1,24 @@
+package main.java.com.lt.core.questions;
+
+public class Temperature {
+
+    public static void main(String... args) {
+
+        temperature(32.0);
+    }
+
+
+    public static void temperature(double celsius) {
+        double fahrenheit;
+        fahrenheit = celsius / 32;
+
+        System.out.println(fahrenheit + " Degree Fahrenheit =   " + celsius + " Degree Celsius");
+
+    }
+}
+
+
+
+
+
+
