643a8402941de22c956c645ffae6b25cae193360
Ankit Chauhan <advik.ankitchauhan85@gmail.com>
2019-03-08 19:03:58 +0530

    change the code

--- java-tutorial/src/main/java/com/lt/core/table/Range.java
+++ java-tutorial/src/main/java/com/lt/core/table/Range.java
@@ -3,12 +3,12 @@ package main.java.com.lt.core.table;
 public class Range {
     public static void main(String... arg){
         for(int range = 1; range <= 100; range = range+1){
-            System.out.println(range);
+
             if (range%2==0){
-                System.out.println("Number is even");
+                System.out.println(range +" " + "Number is even");
             }
             if (range%2!=0){
-                System.out.println("no is odd");
+                System.out.println(range +" " + "no is odd");
             }
         }
     }
