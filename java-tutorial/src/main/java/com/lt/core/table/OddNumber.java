20aa1b6d624bf2118f900bd18687e524887b2fa5
Ankit Chauhan <advik.ankitchauhan85@gmail.com>
2019-03-08 19:00:38 +0530

    change the code

--- java-tutorial/src/main/java/com/lt/core/table/OddNumber.java
+++ java-tutorial/src/main/java/com/lt/core/table/OddNumber.java
@@ -4,6 +4,7 @@ public class OddNumber {
     public static void main (String... arg){
         for (int oddNumber = 1; oddNumber <= 100; oddNumber = oddNumber+2){
             System.out.println(oddNumber);
+
         }
     }
 }
--- java-tutorial/src/main/java/com/lt/core/table/Range.java
+++ java-tutorial/src/main/java/com/lt/core/table/Range.java
@@ -4,6 +4,12 @@ public class Range {
     public static void main(String... arg){
         for(int range = 1; range <= 100; range = range+1){
             System.out.println(range);
+            if (range%2==0){
+                System.out.println("Number is even");
+            }
+            if (range%2!=0){
+                System.out.println("no is odd");
+            }
         }
     }
 }
