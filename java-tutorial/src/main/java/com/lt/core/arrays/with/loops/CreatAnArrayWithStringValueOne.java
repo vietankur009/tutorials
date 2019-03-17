ee0b6a30ae94f684a16687d7bcf8f457530b427e
Ankit Chauhan <advik.ankitchauhan85@gmail.com>
2019-03-17 08:36:51 +0530

    review the code

--- java-tutorial/src/main/java/com/lt/core/arrays/with/loops/CreatAnArrayWithStringValueOne.java
+++ java-tutorial/src/main/java/com/lt/core/arrays/with/loops/CreatAnArrayWithStringValueOne.java
@@ -4,7 +4,7 @@ public class CreatAnArrayWithStringValueOne {
     public static void main(String... arg){
         String [] x = new String[5];
         for(int y = 0; y < x.length; y = y + 1){
-            x [y] = "Anki chauhan , Ankur Chauhan";
+            x [y] = "Ankit chauhan , Ankur Chauhan";
         }
         for (int i = 0; i < x.length; i = i + 1){
             System.out.println(x[i]);
