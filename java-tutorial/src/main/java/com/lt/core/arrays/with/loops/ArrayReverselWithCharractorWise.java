5dc894aac731fa3c5894b1082311a9ecf9b7b764
Ankit Chauhan <advik.ankitchauhan85@gmail.com>
2019-03-18 18:32:43 +0530

    review the code

---
+++ java-tutorial/src/main/java/com/lt/core/arrays/with/loops/ArrayReverselWithCharractorWise.java
@@ -0,0 +1,11 @@
+package main.java.com.lt.core.arrays.with.loops;
+
+public class ArrayReverselWithCharractorWise {
+    public static void main(String... arg){
+        String x = "DHAMPUR IS MY HOME TOWN";
+        String [] k = x.split(" ");
+        for(int c = k.length-1; c >= 0; c = c - 1){
+            System.out.println(k[c]);
+        }
+    }
+}
