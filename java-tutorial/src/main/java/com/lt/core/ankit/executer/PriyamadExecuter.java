907676d87118692fac8ed1a227adaaf5523a1c9e
Ankit Chauhan <advik.ankitchauhan85@gmail.com>
2019-04-04 08:39:05 +0530

    review the code

--- java-tutorial/src/main/java/com/lt/core/ankit/executer/PriyamadExecuter.java
+++ java-tutorial/src/main/java/com/lt/core/ankit/executer/PriyamadExecuter.java
@@ -3,7 +3,7 @@ package main.java.com.lt.core.ankit.executer;
 public class PriyamadExecuter {
     public static void main(String... arg){
         for(int r = 0; r <= 3; r = r + 1){
-            for(int s = 3; s >= r; s = s - 1){
+            for(int s = 3; s > r; s = s - 1){
                 System.out.print(" ");
             }
             for(int c = 0; c <= r; c = c + 1){
