8be038c8a463b0858e2a6b1053aff03efa797643
Ankit Chauhan <advik.ankitchauhan85@gmail.com>
2019-04-14 12:43:56 +0530

    review the code

---
+++ java-tutorial/src/main/java/com/lt/core/looping/WhilePyramidFour.java
@@ -0,0 +1,22 @@
+package main.java.com.lt.core.looping;
+
+public class WhilePyramidFour {
+    public static void main (String... arg){
+        int r = 1;
+        while(r <= 5){
+            int c = 5;
+            while (c >= r){
+                System.out.print(" ");
+                c = c - 1;
+            }
+            int s = 1;
+            while (s <= r){
+                System.out.print(c);
+                System.out.print(" ");
+                s = s + 1;
+            }
+            r = r + 1;
+            System.out.println(" ");
+        }
+    }
+}
